d3b92a3248812c9ff6c98cbd541d36f8dfa7abb3
Faruq <farooqshaikh22@ymail.com>
2024-04-23 23:11:46 +0530

    constants.py corrected

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -1,11 +1,12 @@
 import os,sys
 from datetime import datetime
 
+
 def current_time_stamp():
     
     return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
-CURRENT_TIME_STAMP = current_time_stamp
+CURRENT_TIME_STAMP = current_time_stamp()
 
 ROOT_DIR_KEY = os.getcwd()
 DATA_DIR = 'data'
@@ -15,9 +16,9 @@ ARTIFACT_DIR_KEY = 'artifact'
 
 ## data ingestion related variables
 
-DATA_INGESTION_DIR = 'data_ingestion',
-INGESTED_DATA_DIR = 'ingested_data',
-RAW_DATA_DIR = 'raw_data',
-RAW_DATA = 'data.csv',
-TRAIN_DATA = 'train.csv',
-TEST_DATA = 'test.csv',
+DATA_INGESTION_DIR = 'data_ingestion'
+INGESTED_DATA_DIR = 'ingested_data'
+RAW_DATA_DIR = 'raw_data'
+RAW_DATA = 'data.csv'
+TRAIN_DATA = 'train.csv'
+TEST_DATA = 'test.csv'
