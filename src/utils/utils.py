bf93cbbfc53d392549a5c19aeeb711fad35bccb3
Faruq <farooqshaikh22@ymail.com>
2024-04-26 00:15:54 +0530

    model_evaluate func created in utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,7 @@ import os,sys
 from src.logger.logger import logging
 from src.exception.exception import CustomException
 import pickle
+from sklearn.metrics import r2_score,mean_absolute_error
 
 def save_object(file_path,obj):
     try:
@@ -15,4 +16,31 @@ def save_object(file_path,obj):
         logging.info("error occured while saving an object")
         raise CustomException(e,sys)
         
-    
+        
+## function for evaluating the model    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            
+            ## Train Model
+            model.fit(X_train,y_train)
+            
+            ## Predict testing data
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+            
+            ## Get R2 score for both train and test data
+            train_r2_score = r2_score(y_train)
+            test_r2_score = r2_score(y_test)
+            
+            report[list(models.keys())[i]] = test_r2_score
+            
+            return report
+                    
+        
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
